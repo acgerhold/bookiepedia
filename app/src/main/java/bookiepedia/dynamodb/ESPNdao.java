baa2e3ecbdd7afaed5a1e221fbca8c6a170d1329
acg <acgerhold@gmail.com>
2024-05-23 20:36:48 -0500

    Added class to check data quality of objects created from espn API

--- app/src/main/java/bookiepedia/dynamodb/ESPNdao.java
+++ app/src/main/java/bookiepedia/dynamodb/ESPNdao.java
@@ -1,23 +1,41 @@
 package bookiepedia.dynamodb;
 
+import bookiepedia.dynamodb.models.Event;
 import bookiepedia.dynamodb.models.Schedule;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
 public class ESPNdao {
 
+    private final LocalDateTime now = LocalDateTime.now(ZoneId.of("UTC-05:00"));
+    private final DateTimeFormatter mmddyy = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+    private DataQualityScanner dataQualityScanner;
+
     public JSONObject requestQuery() throws Exception {
 
+        String startDate = "20240523";
+        String endDate = "20240526";
+
         // GET request for NBA schedule
-        String url = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard";
+        // Specify date ranges with '?dates=YYYYMMDD-YYYYMMDD'
+        // No parameters returns schedule for current day
+        String url = String.format("https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard?dates=%s-%s",
+                startDate, endDate);
 
         // Calling the API
         URL obj = new URL(url);
@@ -26,11 +44,11 @@ public class ESPNdao {
         con.setRequestProperty("User-Agent", "Mozilla/5.0");
         int responseCode = con.getResponseCode();
 
-        // Prints response URL and response code
+        // Prints request URL and response code
         System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
+        System.out.println("Response Code : " + responseCode + "\n");
 
-        // Convert response to
+        // Use BufferedReader to build JSON out of InputStream
         BufferedReader br = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
         String inputLine;
@@ -41,11 +59,12 @@ public class ESPNdao {
         // Close connection
         br.close();
 
-        // Return JSONObject as string
+        // Convert
         return new JSONObject(response.toString());
     }
 
-    public Schedule extractSchedule(JSONObject espnResponse) {
+    public String extractSchedule(JSONObject espnResponse) throws JsonProcessingException {
+
         Schedule schedule = new Schedule();
 
         // League ID
@@ -65,25 +84,73 @@ public class ESPNdao {
                 .get()
         );
         // Timestamp
-        schedule.setTimestamp(Stream.of(espnResponse)
-                .map(response -> response.getJSONObject("day"))
-                .map(league -> league.getString("date"))
-                .findFirst()
-                .get()
-        );
+        schedule.setTimestamp(now.toString());
         // Schedule ID
-        schedule.setScheduleId(schedule.getLeagueId() + "-" + schedule.getTimestamp());
-        // Event ID List
+        schedule.setScheduleId(schedule.getLeagueId() + "-" + now.format(mmddyy));
+        // Event ID List *
         schedule.setEventIdList((Stream.of(espnResponse)
                 .map(response -> response.getJSONArray("events"))
                 .map(events -> events.getJSONObject(0)))
                 // How will I adjust this for multiple games?
                 // Couldn't figure out how to iterate through the JSONArray, events, using a stream.
                 // Have to be accessing a JSONObject first (JSONObject(0)) to call getString(key)
+                // Though, might be able to get away with calling JSONObject(0) because 'id' is always the 0th index
                 .map(event -> event.getString("id"))
                 .collect(Collectors.toList())
         );
 
-        return schedule;
+        ObjectMapper mapper = new ObjectMapper();
+
+        String scheduleJson = mapper.writeValueAsString(schedule);
+
+        dataQualityScanner = new DataQualityScanner(scheduleJson);
+        dataQualityScanner.scan();
+
+        return scheduleJson;
+    }
+
+    public String extractEvents(JSONObject espnResponse) throws JsonProcessingException {
+
+        Event event = new Event();
+
+        // Event ID *
+        event.setEventId((Stream.of(espnResponse)
+                .map(r -> r.getJSONArray("events"))
+                .map(es -> es.getJSONObject(0)))
+                .map(e -> e.getString("id"))
+                .findFirst()
+                .get()
+        );
+        // Event Name *
+        event.setEventId((Stream.of(espnResponse)
+                .map(r -> r.getJSONArray("events"))
+                .map(es -> es.getJSONObject(0)))
+                .map(e -> e.getString("name"))
+                .findFirst()
+                .get()
+        );
+        // Event Name (Short) *
+        event.setEventId((Stream.of(espnResponse)
+                .map(r -> r.getJSONArray("events"))
+                .map(es -> es.getJSONObject(0)))
+                .map(e -> e.getString("shortName"))
+                .findFirst()
+                .get()
+        );
+        // Event Headline *
+        event.setEventHeadline(String.valueOf((Stream.of(espnResponse)
+                .map(r -> r.getJSONArray("events"))
+                .map(es -> es.getJSONObject(0)))
+                .map(e -> e.getJSONArray("competitions"))
+                .map(cs -> cs.getJSONArray(0))
+                //.map(c -> c.getJSONObject())
+
+                .findFirst()
+                .get())
+        );
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.writeValueAsString(event);
     }
 }
--- app/src/test/java/bookiepedia/dynamodb/ESPNdaoTest.java
+++ app/src/test/java/bookiepedia/dynamodb/ESPNdaoTest.java
@@ -1,26 +1,36 @@
 package bookiepedia.dynamodb;
 
 import bookiepedia.dynamodb.models.Schedule;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Map;
 
 public class ESPNdaoTest {
 
     @Test
-    public void testingBufferedReaderStream() throws Exception {
+    public void scheduleDataQuality() throws Exception {
+
         ESPNdao espn = new ESPNdao();
         JSONObject result = espn.requestQuery();
 
-        Schedule s = espn.extractSchedule(result);
+        ObjectMapper mapper = new ObjectMapper();
+
+        String str = espn.extractSchedule(result);
+        Schedule s = mapper.readValue(str, Schedule.class);
 
         System.out.println("Schedule Name: " + s.getScheduleName());
         System.out.println("Schedule ID: " + s.getScheduleId());
         System.out.println("League: " + s.getLeagueName() + s.getLeagueId());
         System.out.println("Timestamp: " + s.getTimestamp());
         System.out.println("Events: ");
-        for (String str : s.getEventIdList()) {
-            System.out.println(str);
+        for (String event : s.getEventIdList()) {
+            System.out.println(event);
         }
-
     }
 }
